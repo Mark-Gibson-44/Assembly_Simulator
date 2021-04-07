b711331a1ba4a6851faf8e8a1b3fb2ee0f93604c
mark <mjgibson44@outlook.com>
2021-04-07 13:22:07 +0100

    Added Parser for application to enable traversal through program

---
+++ Parser.cpp
@@ -0,0 +1,91 @@
+#include "Parser.h"
+#include <iostream>
+
+
+void ParseNode::addNode(ParseNode* p)
+{
+	this->children.push_back(p);
+	p->setParent(this);
+}
+
+ParseNode* ParseNode::getParent()
+{
+	return this->Parent;
+}
+
+std::string ParseNode::getLex()
+{
+	return Lex;
+}
+
+void ParseNode::setParent(ParseNode* p)
+{
+	this->Parent = p;
+}
+
+void ParseNode::logNodes()
+{
+	const char* parentInfo = (this->Parent == nullptr) ? "HEAD of Tree" : this->Parent->Lex.c_str();
+	std::cout << "Node: " << this->Lex <<  " with Parent :" << parentInfo << std::endl;
+	for (auto& child : this->children)
+	{
+		child->logNodes();
+	}
+}
+
+
+
+void Parser::match(Tokens t)
+{
+	if (Lexed[current_pos].first == t)
+	{
+		ParseNode* leaf = new ParseNode(Lexed[current_pos].second, (int)Lexed[current_pos].first);
+		Traversal->addNode(leaf);
+		current_pos++;
+		lookahead_pos++;
+	}
+	else
+	{
+		//Throw an error
+	}
+
+}
+
+void Parser::Expr()
+{
+	while (this->lookahead_pos < Lexed.size())
+	{
+		Op();//Currently every expressions begins with an OP
+		//TODO implement Labels
+		
+	}
+	
+}
+
+void Parser::Op()
+{
+	match(Tokens::Op);
+	Traversal = Traversal->children[Traversal->children.size() - 1];//Set Register and either Reg or Imm as children of Opp node
+	match(Tokens::Reg);
+	if (Lexed[current_pos].first != Tokens::Op)
+		RHS();
+	Traversal = Traversal->getParent();//Return to Op Node;
+}
+
+void Parser::RHS()
+{
+	if (Lexed[current_pos].first == Tokens::Imm)
+		match(Tokens::Imm);
+	else
+		match(Tokens::Reg);
+}
+
+ParseNode* Parser::getTree()
+{
+	return head;
+}
+
+void Parser::logTree()
+{
+	head->logNodes();
+}
---
+++ Parser.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <optional>
+#include "Lexer.h"
+#include <memory>
+
+class ParseNode {
+	std::string Lex;
+	int tok;
+	
+	ParseNode* Parent;
+	
+	//ParseNode* getParent();
+	void setParent(ParseNode* p);
+public:
+	ParseNode(std::string l, int v) : Lex(l), tok(v), Parent(nullptr) {}
+	std::vector<ParseNode*> children;
+	void logNodes();
+	void addNode(ParseNode* p);
+	ParseNode* getParent();
+	std::string getLex();
+};
+
+class OpNode: public ParseNode {
+
+	
+	std::optional<std::unique_ptr<ParseNode>> dest;
+
+};
+
+class Parser {
+
+	ParseNode* head = new ParseNode("Program", 3);//Head of a program
+
+	ParseNode* Traversal; // Node ptr to traverse and add nodes accordingly
+	int current_pos; //Current position within the stream of input
+	int lookahead_pos;
+	std::vector<std::pair<Tokens, std::string>> Lexed;// 
+	int ExprNum = 0;
+
+	
+	void match(Tokens t);
+
+	//Recursive parsing functions
+	void Expr();
+	void Op();
+	void Label();
+	void Reg_Reg();
+	void RHS();
+public:
+	ParseNode* getTree();//output
+	void logTree();
+	Parser(std::vector<std::pair<Tokens, std::string>>& in) : Lexed(in), Traversal(head), current_pos(0), lookahead_pos(1) {
+		Expr();
+	}
+
+
+		
+};
